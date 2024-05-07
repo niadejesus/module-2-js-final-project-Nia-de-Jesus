2180275075112204120fedb989832bc9228c290a
niadejesus <susejedain@gmail.com>
2024-05-06 19:10:23 -0700

    third commit

--- BulletCounter.js
+++ BulletCounter.js
@@ -6,7 +6,7 @@ document.addEventListener('keydown', function(event) {
   // Check if the pressed key is the spacebar (keyCode 32 or key " ")
   if (event.keyCode === 32 || event.key === " ") {
     
-    spacebarCount++; // Adds to the counter
+    spacebarCount++; // Adds to the counter by 1 with each spacebar press and then returns the number
     
     document.getElementById('spacebarCount').textContent = spacebarCount; // Updates the displayed number
   }
